742d289a6e220c0551091b3b6e27e8143d7cea80
Tristan Poveda <povedatristan@gmail.com>
2026-01-31 14:01:53 +0100

    Shop
    # Conflicts:
    #	Assets/App/Datas/Masks/1_Healthy_Mask_SSO.asset

---
+++ Assets/App/Scripts/Managers/ShopManager.cs
@@ -0,0 +1,98 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using MVsToolkit.Dev;
+
+namespace GGJ2026
+{
+    public class ShopManager : RegularSingleton<ShopManager>
+    {
+        [Header("Settings")]
+        [SerializeField] private ShopPricing m_Pricing;
+
+        [Header("References")]
+        [SerializeField] private MaskInventory m_Inventory;
+        [SerializeField] private MaskShopSlot[] m_Slots;
+        [SerializeField] private Mask[] m_AvailableMaskPool;
+        [SerializeField] private GameObject m_ShopPanel;
+        
+        private int m_RerollCount;
+        
+        [System.Serializable]
+        public class ShopPricing
+        {
+            public int BaseRerollPrice = 5;
+            public int PriceIncreasePerReroll = 2;
+            private int m_RerollsDone = 0;
+
+            public int GetCurrentPrice() => BaseRerollPrice + (m_RerollsDone * PriceIncreasePerReroll);
+            public void Increment() => m_RerollsDone++;
+        }
+
+        private void Start()
+        {
+            CloseShop();
+        }
+
+        [Button]
+        public void OpenShop()
+        {
+            GameStateManager.Instance.PushContext(GameState.Shop);
+            m_ShopPanel.SetActive(true);
+        }
+
+        [Button]
+        public void CloseShop()
+        {
+            GameStateManager.Instance.PopContext(GameState.Shop);
+            m_ShopPanel.SetActive(false);
+        }
+        
+        public void Reroll()
+        {
+            // TODO: Vérifier la monnaie ici
+            UpdateShop();
+            m_Pricing.Increment();
+        }
+
+        private void UpdateShop()
+        {
+            foreach (var slot in m_Slots)
+            {
+                var randomMask = m_AvailableMaskPool[UnityEngine.Random.Range(0, m_AvailableMaskPool.Length)];
+                
+                int level = GetMaskLevelInInventory(randomMask);
+                slot.Setup(randomMask, level);
+            }
+        }
+        
+        private int GetMaskLevelInInventory(Mask mask)
+        {
+            for (int i = 0; i < MaskInventory.InventorySize; i++)
+            {
+                if (m_Inventory[i] == mask) 
+                    return m_Inventory.GetMaskLevel(i);
+            }
+            return 0;
+        }
+
+        public void OnSlotClicked(MaskShopSlot slot)
+        {
+            Mask mask = slot.CurrentMask;
+            
+            if (m_Inventory.TryGetMask(mask))
+            {
+                m_Inventory.IncreaseMaskLevel(mask);
+                slot.Clear();
+            }
+            else if (m_Inventory.TryAddMask(mask))
+            {
+                slot.Clear();
+            }
+            else
+            {
+                Debug.Log("Inventory Full !");
+            }
+        }
+    }
+}
