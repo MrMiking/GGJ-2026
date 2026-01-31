a09eb356f2bc9e05a1d8f6c049ccec3a18f6e572
Eikins <33116763+Eikins@users.noreply.github.com>
2026-01-31 11:14:43 +0100

    Update Mask SO

--- Assets/App/Scripts/Gameplay/Masks/Mask.cs
+++ Assets/App/Scripts/Gameplay/Masks/Mask.cs
@@ -5,6 +5,24 @@ namespace GGJ2026
     [CreateAssetMenu(fileName = "New Mask", menuName = "GGJ2026/Mask")]
     public sealed class Mask : ScriptableObject
     {
-        [field: SerializeField] public MaskBehaviour BehaviourPrefab { get; private set; }
+        [Header("Properties")]
+        [SerializeField] private MaskRarity m_Rarity;
+        [SerializeField, Range(1, 5)] private int m_MaximumLevel = 5;
+        [SerializeField] private MaskBehaviour m_BehaviourPrefab;
+
+        [Space, Header("Visual")]
+        [SerializeField] private Sprite m_Sprite;
+
+        public MaskRarity Rarity => m_Rarity;
+        public int MaximumLevel => m_MaximumLevel;
+        public MaskBehaviour BehaviourPrefab => m_BehaviourPrefab;
+        public Sprite Sprite => m_Sprite;
+    }
+
+    public enum MaskRarity
+    {
+        Common,
+        Rare,
+        Epic
     }
 }
